32d34da76f35757996b4b1b36de13fb4058aa15e
ChristofferNygren <christoffernygren90@hotmail.com>
2017-05-07 11:59:34 +0200

    Added menus and methods for withdraw and deposit to credit and debit.

--- BankApp/BankApp/Menus.cs
+++ BankApp/BankApp/Menus.cs
@@ -10,34 +10,46 @@ namespace BankApp
     public class Menus
     {
         Menuchoice menuchoice = new Menuchoice();
+
         public void MainMenu()
         {
             Console.WriteLine("Hello and welcome to ACB, How can we be of service today?");
-            Console.WriteLine("Would you like to:\n [1] Set up an account.");
+            Console.WriteLine("Would you like to:\n[1] Set up an account.");
             Console.WriteLine("[2] Manage existing account."); // Change name of existing account holder? Change CardType to existing account? 
             Console.WriteLine("[3] Make a deposit or withdrawl.");
             Console.WriteLine("[4] ");//Om jag kommer på nåt annat
             Console.WriteLine("[5] Exit");
         }
 
-        public void TransactionMenu()
+        public void TransactionMenu(AccountHolder accountholder)
         {
             Console.WriteLine("Would you like to make a:\n[1] Deposit\n[2] Withdraw");
-            menuchoice.TransactionMenuChoice();
-            //Console.WriteLine($"Your total balance is: {account.AccountBalance}");
-            //Console.WriteLine("Press any key to continue...");
-            //Console.ReadKey();
+            menuchoice.TransactionMenuChoice(accountholder);           
         }
-        public void WithdrawMenu()
+        public void WithdrawMenu(AccountHolder accountholder)
         {
             Console.WriteLine("Would you like to withdraw from your\n[1] Debit\n[2] Credit");
-            menuchoice.WithdrawMenuChoice();
-
+            menuchoice.WithdrawMenuChoice(accountholder);
         }
-        public void DepositMenu()
+        public void DepositMenu(AccountHolder accountholder)
         {
                 Console.WriteLine("How much would you like to deposit?");
-                menuchoice.DepositMenuChoice();
+                menuchoice.DepositMenuChoice(accountholder);
+        }
+        public void WithdrawDebitMenu(AccountHolder accountholder)
+        {
+            Console.WriteLine("How much would you like to withdraw?");
+            menuchoice.WithdrawDebitMenuChoice(accountholder);           
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();        
+        }
+        public void WithdrawCreditMenu(AccountHolder accountholder)
+        {
+            Console.WriteLine("How much would you like to withdraw?");
+            menuchoice.WithdrawCreditMenuChoice(accountholder);            
+            Console.WriteLine("Our current rate is 5%");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
         public void ManageAccount(AccountHolder a)
         {
